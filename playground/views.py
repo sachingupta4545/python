e88830f38744e89f18547c347af1717af914b92d
sachingupta4545 <sachin.gupta6292@gmail.com>
2025-07-10 08:54:13 +0530

    sdf

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# def say_hello(request):
+#     return HttpResponse("Hello, world! This is a response from the playground app.")
+
 def say_hello(request):
-    return HttpResponse("Hello, world!")
+    name = request.GET.get('name', 'Guest')  # default to 'Guest' if not provided
+    return HttpResponse(f"Hello, {name}!")
 
 # Create your views here.
